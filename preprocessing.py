ac266b9981e5b5b0394f21a3dd1cf88f87072b2d
Leo <38267386+randomlylelo@users.noreply.github.com>
2023-03-10 14:34:49 -0800

    some progress

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,19 @@
+# 
+# %% Download the CMU dict
+import nltk
+nltk.download('cmudict')
+from nltk.corpus import cmudict
 
-print('hello world')
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+DATA_PATH = 'data/Syllable_dictionary.txt'
+
+data = pd.read_fwf(DATA_PATH, widths=10)
+
+
+#%%
+
+len(cmudict.dict())
