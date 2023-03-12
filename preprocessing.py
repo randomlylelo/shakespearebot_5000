6fde4cff3dce72a02a3b27cf4228b897138105a1
Leo <38267386+randomlylelo@users.noreply.github.com>
2023-03-11 17:14:18 -0800

    generate the sonnet works

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ import re
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 import nltk
 nltk.download('cmudict')
@@ -118,7 +119,105 @@ HMM = unsupervised_HMM(X, num_hidden_states, N_iters, seed=None)
 
 # %%
 # create sonnet
-for _ in range(14):
-  print(sample_sentence(HMM, X_map, n_words=10))
+# for _ in range(14):
+#   print(sample_sentence(HMM, X_map, n_words=10))
+
+# %%
+rhyme_groups = {}
+for _ in tqdm(range(1000)):
+    line = sample_sentence(HMM, X_map, n_words=10)
+    word_list = line.split(" ")
+    last_word = word_list[len(word_list) - 1]
+    if (len(last_word) < 2):
+        continue
+
+    last_2_letters = last_word[len(last_word)-2:]
+    if last_2_letters not in rhyme_groups:
+        rhyme_groups[last_2_letters] = []
+    rhyme_groups[last_2_letters].append(line)  # getting the last 2 letters and using it as a dictionary key
+
+print(rhyme_groups)
+
+
+
+#%%
+# get rhyming to work
+to_print = []
+last_word_set = set()
+
+for key in rhyme_groups:
+    lst = rhyme_groups[key]
+    if len(lst) > 2:
+        counter = 0
+        # deal with the case where there is no two
+        to_add_to_print = []
+        for sentence in lst:
+            last_word = sentence.split()[-1]
+            if last_word not in last_word_set:
+                counter += 1
+                to_add_to_print.append(sentence)
+                last_word_set.add(last_word)
+
+            if counter == 2:
+                for str in to_add_to_print:
+                    to_print.append(str)
+                break
+
+    if len(to_print) == 14:
+        break
+
+print(to_print[0]) # a
+print(to_print[2]) # b
+print(to_print[1]) # a
+print(to_print[3]) # b
+print()
+
+print(to_print[4]) # c
+print(to_print[6]) # d
+print(to_print[5]) # c
+print(to_print[7]) # d
+print()
+
+print(to_print[8]) # e
+print(to_print[10]) # f
+print(to_print[9]) # e
+print(to_print[11]) # f
+print()
+
+print(to_print[12]) # g
+print(to_print[13]) # g
+
+#%%
+
+# Try to get CMU rhyme dictionary
+pronouce_dct = cmudict.dict()
+cmu_rhyme_groups = {}
+for _ in tqdm(range(1000)):
+    line = sample_sentence(HMM, X_map, n_words=10)
+    word_list = line.split(" ")
+    last_word = word_list[len(word_list) - 1]
+    if (len(last_word) < 2):
+        continue
+
+    if last_word in pronouce_dct:
+        # get last couple pronounation, since we can't use list, use str rep
+        pronouce = repr(pronouce_dct[last_word][0][-2:])
+        if pronouce not in cmu_rhyme_groups:
+            cmu_rhyme_groups[pronouce] = []
+        cmu_rhyme_groups[pronouce].append(line)
+
+print(cmu_rhyme_groups)
+
+# to_print = []
+# for key in cmu_rhyme_groups:
+#     lst = cmu_rhyme_groups[key]
+#     if len(lst) > 2:
+#         counter = 0
+#         for sentence in lst:
+#             counter += 1
+#             to_print.append(sentence)
+
+#         if counter == 2:
+#             continue
 
 # %%
